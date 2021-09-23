8b46c85639d6073625c7a808115ecac1fc44f520
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-23 15:40:56 +0200

    add snack3

--- inc/functions.php
+++ inc/functions.php
@@ -4,13 +4,14 @@
  * @param n:Integer max of elements
  * @return  Array:Integers from 1 to 100
  */
-function getArrayRandNumber($n){
-    $arr=[];
-    $num_rand=0;
-    for( $i=0; $i<=$n; $i++){
-        $num_rand=rand(1,100);
-        if(!in_array($num_rand,$arr)){
-            $arr[$i]=$num_rand;
+function getArrayRandNumber($n)
+{
+    $arr = [];
+    $num_rand = 0;
+    for ($i = 0; $i <= $n; $i++) {
+        $num_rand = rand(1, 100);
+        if (!in_array($num_rand, $arr)) {
+            $arr[$i] = $num_rand;
         }
     }
     return $arr;
@@ -20,13 +21,25 @@ function getArrayRandNumber($n){
  * @param arr:Array Integer 
  * @return Integer  Arihmetical-Mean
  */
-function getMedia($arr){
-    $len=count($arr);
-    $sum=0;
-    for($i=0; $i<$len; $i++){
-        $sum+=$arr[$i];
-
+function getMedia($arr)
+{
+    $len = count($arr);
+    $sum = 0;
+    for ($i = 0; $i < $len; $i++) {
+        $sum += $arr[$i];
     }
-     return floor($sum /$len);
-    
+    return floor($sum / $len);
+}
+/**
+ * @param $name String 
+ * @param $mail String 
+ * @param $age Int 
+ * @return String 
+ */
+function chekEmail($name, $mail, $age)
+{
+    if (strlen($name) > 3 && strpos($mail, ".") && strpos($mail, "@")  && is_numeric($age)) {
+        return "Accesso Riuscito";
+    }
+    return "Accesso negato";
 }
